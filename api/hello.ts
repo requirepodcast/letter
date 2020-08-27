926977d8515126230a937ca3789241302ef2307b
Adam Siekierski <a@siekierski.ml>
2020-08-27 11:32:15 +0200

    Basics of email send endpoint

--- api/hello.ts
+++
@@ -1,6 +0,0 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-
-export default (req: NowRequest, res: NowResponse) => {
-  res.statusCode = 200;
-  res.json({ name: "John Doe" });
-};
---
+++ pages/api/send.ts
@@ -0,0 +1,33 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import remark from "remark";
+import html from "remark-html";
+import pug from "pug";
+
+export default (req: NowRequest, res: NowResponse) => {
+  if (req.headers.api_token !== process.env.API_TOKEN) {
+    return res.status(401).json({ error: "Not authenticated" });
+  }
+
+  const errors = [];
+  if (!req.body.content) {
+    errors.push({ error: "No content provided" });
+  }
+  if (!req.body.title) {
+    errors.push({ error: "No title provided" });
+  }
+  if (errors.length !== 0) {
+    return res.status(400).json({ errors });
+  }
+
+  remark()
+    .use(html)
+    .process(req.body.content, (err, file) => {
+      if (err) {
+        return res.status(400).json({ errors: [{ error: `Can't process provided markdown` }] });
+      }
+
+      const letterHtml = pug.renderFile("templates/mailTemplate.pug", { content: file });
+
+      return res.json({ message: "Email successfully sent", html: letterHtml });
+    });
+};
